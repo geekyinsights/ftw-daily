771cbfa3351feb07e3f5ea53a0496eb9c043fc48
Christina <geekyinsights@users.noreply.github.com>
2020-03-01 23:12:59 -0600

    'page with product but didmount doens't work'

--- src/containers/ListingPage/SectionProducts.js
+++ src/containers/ListingPage/SectionProducts.js
@@ -10,13 +10,14 @@ import css from './ListingPage.css';
    
 import helsinkiImage from '../../assets/bestthing_smaller.png';
    
-   
+
 class ProductImage extends Component {
     render() {
        const { alt, ...rest } = this.props;
        return <img alt={alt} {...rest} />;
     }
    }
+
 const LazyImage = lazyLoadWithDimensions(ProductImage);
    
 const productLink = (name, image) => {
@@ -37,21 +38,30 @@ const productLink = (name, image) => {
        </NamedLink>
      );
 };
-
-const state = {    
+const state = {   
     //put api keys below
-    //Accress token
-    //client_id
+    ACCESS_TOKEN: 'bfv7raui86hh86d62v3ms02mndpcq6d',
+    CLIENT_ID: 'j0dd7uh4z9awmvfzhpksn3m3eunwkim'
   
 };
+
+ 
+componentDidMount() {
+    var data = JSON.stringify({});
   
-  componentDidMount() {
-      var data = JSON.stringify({});
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
   
-      var xhr = new XMLHttpRequest();
-      xhr.withCredentials = true;
-  
-      xhr.addEventListener("readystatechange", function () {
+
+    const storeUrl = new URL('https://api.bigcommerce.com/stores/8l7i9q0z6s/v3/');
+
+    // Use the store's canonical URL which should always resolve
+    
+    document.getElementById("page-origin").innerHTML = pageUrl.origin;
+    const graphQLUrl = `${storeUrl.origin}/graphql`;
+    const addToCartURLFormat = `{this.responseText.site.settings.url.vanityUrl}/cart.php?action=add&product_id=`
+
+    xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
           console.log(this.responseText); /// This is where I get the storefront key
   
@@ -62,106 +72,104 @@ const state = {
             token: null
         };
   
-  
-          /*
-              API fetching
-          */
-         function getProductAndSiteInfo(params) {
+        //API fetching
+        function getProductAndSiteInfo(params) {
             const storeUrl = new URL(params.store_url);
-  
+    
             // Use the store's canonical URL which should always resolve
             const graphQLUrl = `${storeUrl.origin}/graphql`;
-  
+    
             // Set up GraphQL query
             // If specific product IDs were supplied, fetch them, else just get the first few products
             const graphQLQuery = `
                 query StaticSiteExample {
-                  customer {
-                    firstName
-                    lastName
-                    email
-                  }
-                          site {
-                            products${params.product_ids ? `(entityIds:[${params.product_ids}])` : ''} {
-                              edges {
-                                product: node {
-                                  ...ProductFields
+                    customer {
+                        firstName
+                        lastName
+                        email
+                    }
+                    site {
+                        products (first: 6){
+                        edges {
+                            product: node {
+                                ...ProductFields
                                 }
-                              }
                             }
-                          settings {
+                        }
+                        settings {
                             storeName
                             url {
-                              vanityUrl
+                                vanityUrl
+                                }
                             }
-                          }
                         }
-                      }
-                  fragment ProductFields on Product {
-                    id
-                    entityId
-                    name
-                    sku
-                    path
-                    description
-                    defaultImage {
-                      img320px: url(width: 320)
-                      img640px: url(width: 640)
-                      img960px: url(width: 960)
-                      img1280px: url(width: 1280)
-                      altText
-                    }
-                      prices {
-                        price {
-                          value
-                          currencyCode
                         }
-                    retailPrice {
-                      value
-                      currencyCode
+                    fragment ProductFields on Product {
+                        id
+                        entityId
+                        name
+                        sku
+                        path
+                        description
+                        defaultImage {
+                        img320px: url(width: 320)
+                        img640px: url(width: 640)
+                        img960px: url(width: 960)
+                        img1280px: url(width: 1280)
+                        altText
+                        }
+                        prices {
+                            price {
+                            value
+                            currencyCode
+                            }
+                        retailPrice {
+                        value
+                        currencyCode
+                        }
                     }
-                  }
-                }`
+                    }`
+    
+            // Fetch data from the GraphQL Storefront API
+            return fetch(graphQLUrl, {
+                method: 'POST',
+                credentials: 'include',
+                mode: 'cors',
+                headers: { 'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${params.token}`},
+                body: JSON.stringify({ query: graphQLQuery}),
+            })
+            .then(res => res.json())
+            .then(res => res.data);
+        }
+    
+    
+        // It seems like the required parameters were supplied, try to load the product data from the Storefront API
+        getProductAndSiteInfo(params).then(data => {
+            // With our data loaded, render the product listing
+            console.log(data);
+            this.setState({
+                responseText: data
+            });
+    
+            }).catch(e => {
+            // Some error was encountered
+            console.log(e);
+            throw(e);
+            });
+         }
+         
+    
   
-          // Fetch data from the GraphQL Storefront API
-          return fetch(graphQLUrl, {
-            method: 'POST',
-            credentials: 'include',
-            mode: 'cors',
-            headers: { 'Content-Type': 'application/json',
-                     'Authorization': `Bearer ${params.token}`},
-            body: JSON.stringify({ query: graphQLQuery}),
-          })
-          .then(res => res.json())
-          .then(res => res.data);
-      }
+    xhr.open("POST", "https://api.bigcommerce.com/stores/store_hash/v3/storefront/api-token");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("x-auth-client", CLIENT_ID);
+    xhr.setRequestHeader("x-auth-token", ACCESS_TOKEN);
   
-  
-      // It seems like the required parameters were supplied, try to load the product data from the Storefront API
-      getProductAndSiteInfo(params).then(data => {
-        // With our data loaded, render the product listing
-          console.log(data);
-          this.setState({
-            responseText: data
-          });
-  
-        }).catch(e => {
-          // Some error was encountered
-          console.log(e);
-          throw(e);
-        });
-  
-            }
-          });
-  
-          xhr.open("POST", "https://api.bigcommerce.com/stores/store_hash/v3/storefront/api-token");
-          xhr.setRequestHeader("content-type", "application/json");
-          xhr.setRequestHeader("x-auth-client", "j0dd7uh4z9awmvfzhpksn3m3eunwkim");
-          xhr.setRequestHeader("x-auth-token", "bfv7raui86hh86d62v3ms02mndpcq6d");
-  
-          xhr.send(data);
+    xhr.send(data);
    }
-  
+}
+
 const SectionProducts = props => {
     const { rootClassName, className } = props;
     
@@ -169,28 +177,30 @@ const SectionProducts = props => {
    
     return (
        <div className={classes}>
-           <div className={css.title}>
+           <div className={css.productTitle}>
                <FormattedMessage id="SectionProducts.title" />
            </div>
    
            <div className={css.card}>
-               <div className={css.cardImgTop} >
+               <div className={css.cardImgTop}>
                  <img loading='lazy' src='../../assets/bestthing_smaller.png'/>
-                       {/*1. Write html product boxes as a list 
-                2. React JSX, link html to state
-                <h2>It is {this.state.responseText.productNmae()}.</h2> */
+               
                        <div className={css.cardBody}>
-                           <h5 className={css.cardTitle}>'Test' 'Test'</h5>
-                           <p className={css.cardText}>test</p>
-                       
-                       <a href="" >Add to cart</a>
+                        <h5 classNames={css.cardTitle}>{this.responseText.product.name}</h5>
+                        <h5 className={css.cardPrice}>{this.responseText.product.price}</h5>
+                            <p className={css.cardDescription}>{this.responseText.product.description}</p>
+                            
+                            <a href="this.responseText.product.entityId" className={css.enquirySubmitButtonWrapper }>Add to cart</a>
                        </div>
-                   </div>`
+                </div>
            </div>
        </div>
      );
-   };
-   
+
+
+
+}
+
 SectionProducts.defaultProps = { rootClassName: null, className: null };
    
 const { string } = PropTypes;
